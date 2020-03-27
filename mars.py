7f64ba4893814137cb6a31997d66b2aa40450561
liuly12 <leyang.liu@hotmail.com>
2020-03-27 11:27:03 +0000

    Start notes on Mars as a base

---
+++ mars.py
@@ -0,0 +1,14 @@
+print("Hello, I am Count Dracula!")
+print("My friends and I are interested in colonising Mars.")
+
+colour = "red"
+temperature = -60
+humidity = 80
+moons = 2
+
+if colour == "red":
+   print("Good, my favourite colour!")
+else:
+   print("Colour is irrelevant.")
+
+
